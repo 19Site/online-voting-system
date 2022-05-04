566f93ded5ed1c45f0c4ec6e29a8472f3d30b7ff
user <user@example.com>
2022-05-04 07:45:08 +0000

    Updated backend create campaign dto validation

--- backend/src/campaigns/dto/create-campaign.dto.ts
+++ backend/src/campaigns/dto/create-campaign.dto.ts
@@ -13,11 +13,11 @@ export class CreateCampaignDto {
 
     @ApiProperty({ description: 'start at' })
     @IsNotEmpty()
-    @Matches(/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/, { message: 'invalid format of start at' })
+    @Matches(/^[0-9]{4}-[0-9]{2}-[0-9]{2}(\T| )[0-9]{2}:[0-9]{2}:[0-9]{2}(|\.[0-9]{3}Z)$/, { message: 'invalid format of start at' })
     startAt: Date;
 
     @ApiProperty({ description: 'end at' })
     @IsNotEmpty()
-    @Matches(/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/, { message: 'invalid format of end at' })
+    @Matches(/^[0-9]{4}-[0-9]{2}-[0-9]{2}(\T| )[0-9]{2}:[0-9]{2}:[0-9]{2}(|\.[0-9]{3}Z)$/, { message: 'invalid format of end at' })
     endAt: Date;
 }
