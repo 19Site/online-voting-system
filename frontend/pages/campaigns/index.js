db5916ba83c0f0f6de7b2a21d68ecdbe92e5ad80
user <user@example.com>
2022-05-04 16:21:02 +0000

    Updated frontend

--- frontend/pages/campaigns/index.js
+++ frontend/pages/campaigns/index.js
@@ -8,6 +8,8 @@ import { useEffect, useState } from 'react';
 
 import Axios from 'axios';
 
+import Moment from 'moment';
+
 export default function Page() {
 
 	// campaigns
@@ -62,26 +64,62 @@ export default function Page() {
 
 						campaigns.length > 0 ? campaigns.map((campaign, i) => {
 
+							// start at
+							const startAt = Moment(campaign.startAt).isValid() ? Moment(campaign.startAt).format('LLL') : '---';
+
+							// end at
+							const endAt = Moment(campaign.endAt).isValid() ? Moment(campaign.endAt).format('LLL') : '---';
+
 							// return
 							return (
 
 								<div key={'campaign-index-' + i}>
 
-									<div className='mt-4'>
+									<div
 
-										<Link
+										className='mt-4 py-4'
 
-											href={{
+										style={{
 
-												pathname: '/campaigns/' + campaign.id
-											}}
-										>
+											borderTop: 'solid 1px #EEEEEE'
+										}}
+									>
 
-											<a className='text-decoration-none'>
+										<div className='fw-bold'>
 
-												Voting Campaign {i + 1} : {campaign.name}
-											</a>
-										</Link>
+											Voting Campaign {i + 1} : {campaign.name}
+										</div>
+
+										<div className='text-secondary small mt-1'>
+
+											<span>
+
+												Start: {startAt}
+											</span>
+
+											<span className='ms-3'>
+
+												End: {endAt}
+											</span>
+										</div>
+
+										{ // campaigns options
+
+											campaign.campaignOptions.map((option, i2) => {
+
+												// return
+												return (
+
+													<div key={'campaign-index-' + i + '-options-' + i2}>
+
+														<div className='ms-4 mt-1'>
+
+															{i2 + 1}. {option.name}
+														</div>
+													</div>
+												);
+											})
+										}
 									</div>
 								</div>
 							);
